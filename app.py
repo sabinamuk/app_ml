06a9165956088ad759fc75abf241ae95835fa4f1
sabinamuk <stm1573@icloud.com>
2026-06-03 15:14:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ except ImportError:
 # Запуск обучения, если моделей нет
 if not os.path.exists('models/linear.pkl'):
     st.warning("Модели не найдены. Запускаю процесс обучения...")
+    result = subprocess.run(['python', 'train_models.py'], capture_output=True, text=True)
+    if result.returncode != 0:
+        st.error("Ошибка при обучении моделей:")
+        st.code(result.stderr) # Здесь мы увидим, что именно сломалось!
+        st.stop() # Останавливаем выполнение, чтобы не идти дальше
+    else:
+        st.success("Обучение завершено!")
     subprocess.run(['python', 'train_models.py'], check=True)
     st.success("Обучение завершено!")
     st.rerun()
